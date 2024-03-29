c2fbb2d7e85a55e45bc281894df2137ddfc0b07b
Sahil-Rathod <sahilrathod2810@gmail.com>
2024-03-29 15:45:43 +0530

    solved the bugs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,15 +2,16 @@ async function main() {
   const Voting = await ethers.getContractFactory("Voting");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const Voting_ = await Voting.deploy(["Mark", "Mike", "Henry", "Rock"], 90);
+  const Voting_ = await Voting.deploy(
+    ["BJP", "Congress", "AAP", "Shivsena"],
+    90
+  );
   console.log("Contract address:", Voting_.address);
-
-
 }
 
 main()
- .then(() => process.exit(0))
- .catch(error => {
-   console.error(error);
-   process.exit(1);
- });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-
+import img from "./votingbg.png";
 const Login = (props) => {
-    return (
-        <div className="login-container">
-            <h1 className="welcome-message">Welcome to decentralized voting application</h1>
-            <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
-        </div>
-    )
-}
+  return (
+    <div className="login-container">
+      <div
+        class="image"
+        style={{
+          backgroundImage: { img },
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h1 className="welcome-message">
+          Welcome to JSPM's Decentralized Voting System
+        </h1>
+        <button className="login-button" onClick={props.connectWallet}>
+          Login Metamask
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
